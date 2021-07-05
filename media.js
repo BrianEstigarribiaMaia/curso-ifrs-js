e1c644dc16b993846ce7591376d0dca7e5f76dee
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-07-05 19:28:54 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,9 @@
+var nota1 = 8;
+var nota2 = 7;
+var media = (nota1 + nota2)/2;
+
+if(media >= 7){
+	alert("Média "+ media +" aprovado")
+}else{
+	alert("Média "+ media +" reprovado")
+}
---
+++ meucodigo.js
@@ -0,0 +1,9 @@
+var a = 12;
+var b = 3;
+var c = a + b;
+
+alert("Total da soma " + c);
+
+var nome;
+nome = prompt("Informe o seu nome: ");
+alert("Olá " + nome);
