2c0556d72e582e5dfea20a977d4e172500b967f8
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-07-06 17:25:44 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,9 @@
+function umClique(){
+	document.getElementById("imagem").src = "fachada.jpg";
+	document.getElementById("log").innerHTML += "FOTO DA FACHADA<br>";
+}
+
+function doisCliques(){
+	document.getElementById("imagem").src = "ifrs.jpg";
+	document.getElementById("log").innerHTML += "FOTO DO LOGO<br>";
+}
