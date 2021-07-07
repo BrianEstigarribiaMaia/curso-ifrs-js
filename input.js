2c81203e7301eca37a08ab147f2794ef07cab2ae
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-07-07 16:55:10 -0300

    Add files via upload

---
+++ input.js
@@ -0,0 +1,32 @@
+function submeter(){
+	document.getElementById("log").innerHTML += "Submetido<br>";
+	return false;
+}
+
+function resetar(){
+	document.getElementById("log").innerHTML += "Resetado<br>";
+}
+
+function sairDoCampo(){
+	document.getElementById("log").innerHTML += "Saiu do campo<br>";
+}
+
+function mudar(){
+	document.getElementById("log").innerHTML += "Mudou os dados<br>";
+}
+
+function entrarNoCampo(){
+	document.getElementById("log").innerHTML += "Entrou no campo<br>";
+}
+
+function selecionar(){
+	document.getElementById("log").innerHTML += "Selecionou dados<br>";
+}
+
+function teclaBaixo(){
+	document.getElementById("log").innerHTML += "Teclou para baixo<br>";
+}
+
+function teclaCima(){
+	document.getElementById("log").innerHTML += "Teclou para cima<br>";
+}
